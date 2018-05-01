95265bf88392500dfcb2277a8c32a614217f7ada
Alia Awni <aawni@ucsd.edu>
2018-05-01 16:15:25 -0700

    created post method for adding to map db

--- app.js
+++ app.js
@@ -16,13 +16,10 @@ const app = express();
 var handlebars = require('express3-handlebars');
 
 
-
-
 // our routes
 const home = require('./routes/home');
 const addSpeed = require('./routes/addSpeed');
 const titlePage = require('./routes/titlePage');
-
 
 
 // all environments
@@ -40,10 +37,10 @@ app.use(express.cookieParser('IxD secret key'));
 app.use(express.session());
 app.use(app.router);
 
+
 // put all of your static files (e.g., HTML, CSS, JS, JPG) in the public/
 // sub-directory, and the server will serve them from there.
 // Learn more: http://expressjs.com/en/starter/static-files.html
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 
@@ -58,51 +55,12 @@ app.get('/getplaces', home.places);
 app.get('/getnetworks', home.networks);
 
 app.get('/addSpeed', addSpeed.view);
+app.post('/add', addSpeed.add);
 
 app.get('/titlePage', titlePage.view);
 
-// Sends list data
-app.get('/getListData', function(req, res) {
-  console.log('sending list data: ', fakeListDatabase);
-  res.send(fakeListDatabase);
-});
-
-// Sends map marker data
-app.get('/getMapData', function(req, res) {
-  console.log('sending map data: ', fakeMapDatabase);
-  res.send(fakeMapDatabase);
-});
 
 
-
-// this fake database will be reset when this script restarts.
-// however, as long as the script is running, this database can be modified at will.
-
-/*const fakeListDatabase = {
-  'Starbucks': {title:'Starbucks', img: 'images/starbucks.jpg', speed: '51.2 Mbsp'},
-  'Geisel Library': {title:'Geisel Library', img: 'images/geisel.jpg', speed: '48.62 Mbsp'},
-  'Center Hall': {title:'Center Hall', img: 'images/center_hall.jpg', speed: '29.9 Mbsp'},
-  'Commuter Lounge': {title:'Commuter Lounge', img: 'images/commuter_lounge.jpg', speed: '17.3 Mbsp'},
-};*/
-
-//old implementation
-// TODO: make the color category depend on the speed
-// TODO: add other fields we want to store (speed, date and time, network)
-/*const fakeMapDatabase = [
-  {
-    position: {lat: 32.88121, lng: -117.237449},
-	color: 'green'
-  }, {
-	position: {lat: 32.880097, lng: -117.236431},
-	color: 'yellow'
-  }, {
-	position: {lat: 32.880984, lng: -117.237821},
-	color: 'red'
-  }, {
-	position: {lat: 32.879323, lng: -117.237261},
-	color: 'red'
-  }
-];*/
 
 
 app.listen(app.get('port'), () => {
--- routes/addSpeed.js
+++ routes/addSpeed.js
@@ -1,8 +1,37 @@
+// use this library to interface with SQLite databases: https://github.com/mapbox/node-sqlite3
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('markers.db');
 
-/*
- * GET addSpeed page.
-*/
 
-exports.view = function(req, res){
+// GET addSpeed page
+exports.view = function(req, res) {
   res.render('addSpeed');
+};
+
+// add a speed to the database
+// TODO: this method should also update any entries in the places
+// table which are close enough to this location by adding it into 
+// that place's average speed
+exports.add = function(req, res) {
+  const date = new Date();
+  const sqllite_date = date.toISOString();
+
+  db.run(
+    'INSERT INTO networks VALUES ($lat, $lng, $speed, $date)',
+    // parameters to SQL query:
+    {
+      $lat: req.body.lat,
+      $lng: req.body.lng,
+      $speed: req.body.speed,
+      $date: sqllite_date,
+    },
+    // callback function to run when the query finishes:
+    (err) => {
+      if (err) {
+        res.send({success: false});
+      } else {
+        res.send({success: true});
+      }
+    }
+  );
 };
