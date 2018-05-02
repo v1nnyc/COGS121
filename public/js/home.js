ed76176ea10dbfdd276e78058cfd67b21b139670
Alia Awni <aawni@ucsd.edu>
2018-05-02 00:19:15 -0700

    display the user's current internet speed if successful

--- public/js/home.js
+++ public/js/home.js
@@ -92,29 +92,29 @@ function addSpeed() {
         };
 
         doAjaxPost('/add', data, (response) => {
-          console.log('in callback');
-          giveConfirmation(response.success, '');
+          giveConfirmation(response.success, data.speed, '');
         });
       },
       // call this function if failed to get location
       () => {
-        giveConfirmation(false, 'Unable to retrieve your location.')
+        giveConfirmation(false, 0, 'Unable to retrieve your location.')
       });
   } else {
     // call this function if we can't get internet speed from browser
-    giveConfirmation(false, 'This is not supported by your browser, please use Chrome.');
+    giveConfirmation(false, 0, 'This is not supported by your browser, please use Chrome.');
   }
     
 }
 
 
 // closes the add speed popup, opens the confirmation popup and populates text accordingly
-function giveConfirmation(successful, errorMessage) {
+function giveConfirmation(successful, speed, errorMessage) {
   $('#add-speed-container').hide();
   $('#confirmation-container').show();
   if (successful) {
     $('#confirmation-title').text('Success!');
-    $('#confirmation-text').text('You data has been added to our map. Thank you for your help!');
+    $('#confirmation-text').text('You data has been added to our map. ' +
+      'Your internet speed is currently ' + speed + ' Mbps. Thank you for your help!');
   } else {
     $('#confirmation-title').text('Failed to add your internet speed');
     $('#confirmation-text').text('Error: ' + errorMessage);
