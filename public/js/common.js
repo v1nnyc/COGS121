297ae91c6acc6062b7b5b720c0f4a61822e38822
Alia Awni <aawni@ucsd.edu>
2018-05-08 12:04:55 -0700

    set up for being able to reorder list items

--- public/js/common.js
+++ public/js/common.js
@@ -7,15 +7,23 @@ function checkForCurrentLocation(successFunction, failureFunction) {
   }
   else {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        console.log("Got position data: Lat = " + position.coords.latitude + 
-                    ", Lng = " + position.coords.longitude);
-        window.localStorage.setItem("latitude", position.coords.latitude);
-        window.localStorage.setItem("longitude", position.coords.longitude);
-        successFunction({latitude: position.coords.latitude, 
-                        longitude: position.coords.longitude});
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          console.log("Got position data: Lat = " + position.coords.latitude + 
+                      ", Lng = " + position.coords.longitude);
+          window.localStorage.setItem("latitude", position.coords.latitude);
+          window.localStorage.setItem("longitude", position.coords.longitude);
+          successFunction({latitude: position.coords.latitude, 
+                          longitude: position.coords.longitude});
+        }, 
+        // This function will be called if permission is denied.
+        (error) => {
+          console.log("Failed to get current location information.");
+          failureFunction();
       });
-    } else {
+    } 
+    // this will be called if the browser doesn't support geolocation
+    else {
       console.log("Failed to get current location information.");
       failureFunction();
     }
@@ -48,6 +56,23 @@ function doAjaxPost(url, data, callbackFunc) {
       callbackFunc(response);
     },
   });
+}
+
+function calcColor(x) {
+  if(x < 5) return 'red';
+  else if(x < 25) return 'yellow';
+  else return 'green';
+}
+
+function calcColorIcon(c) {
+  switch (c) {
+    case 'red':
+      return 'images/red-marker.png';
+    case 'yellow':
+      return 'images/yellow-marker.png';
+    default:
+      return 'images/green-marker.png';
+  };
 }
 
 // define a generic Ajax error handler:
--- public/js/home.js
+++ public/js/home.js
@@ -1,19 +1,9 @@
-$(document).ready(() => {
-  // fill list results
-  doAjaxGet('/getMarkers', (data) => {
-    data.forEach(place => {
-      //require an image and a speed to add
-      if(place.image && place.speed){
-        //creating new object
-        const obj = {title: place.name,
-                    img: place.image,
-                    speed: place.speed,
-                    color: calcColor(place.speed)};
-        $('#list-results').append(createListElementFromObj(obj));
-      }
-    });
-  });
+// required for reordering list
+let listItems = [];
+let markers = [];
+let map = null;
 
+$(document).ready(() => {
   // When the user clicks contribute, open add speed popup
   $('#contribute').click(() => { 
       $('#add-speed-container').show();
@@ -24,27 +14,24 @@ $(document).ready(() => {
       $('.popup-container').hide();
   });
 
+  // function located in js/addSpeed.js
   $('#add-speed').click(addSpeed);
 
 });
 
 
-// This function gets called when Google maps API finishes checking our API key
-// (passed in through the script tag in home.html)
+// This function gets called when Google maps API 
+// finishes checking our API key (passed in through
+// the script tag in home.html)
 function initMap() {
 
-  // center around the location of Geisel
-  const uluru = {lat: 32.881214, lng: -117.237449};
-  const map = new google.maps.Map(document.getElementById('map'), {
+  // center map around Geisel
+  const center = {lat: 32.881214, lng: -117.237449};
+  //store map as global variable
+  map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
-    center: uluru
+    center: center
   });
-
-  const icons = {
-    red: {icon: 'images/red-marker.png'},
-    yellow: {icon: 'images/yellow-marker.png'},
-    green: {icon: 'images/green-marker.png'}
-  };
 
   // add current location marker if current location is available
   checkForCurrentLocation((position) => {
@@ -53,7 +40,9 @@ function initMap() {
       icon: "images/current-location.png",
       map: map
     });
-  });
+  }, 
+    // TODO: if location is not available should grey out "Closest" filter
+    () => {});
 
   // create internet speed dots on map
   doAjaxGet('/getDots', (dots) => {
@@ -61,82 +50,62 @@ function initMap() {
       if(feature.lat && feature.lng && feature.speed){
         const marker = new google.maps.Marker({
           position: {lat: feature.lat, lng: feature.lng},
-          icon: icons[calcColor(feature.speed)].icon,
+          icon: calcColorIcon(calcColor(feature.speed)),
           map: map
         });
       }
     });
   });
 
+  getListResults();
+
+}
+
+// should only be called once when the initMap function is called
+function getListResults() {
+  doAjaxGet('/getMarkers', (data) => {
+    data.forEach(marker => {
+        marker['color'] = calcColor(marker.speed);
+        listItems.push(marker);
+    });
+    // reorderListItems(true);
+    createListFromObjs();
+    createMarkersFromObjs();
+  });
 }
 
 
-function calcColor(x) {
-  if(x < 5) return 'red';
-  else if(x < 25) return 'yellow';
-  else return 'green';
-}
-
-
-// Attempts to add the user's current location and internet speed to database
-function addSpeed() {
-  // TODO: use a more robust method for getting internet speed
-  if (navigator.connection) {
-    checkForCurrentLocation(
-      //call this fuction if get location successfully
-      (position) => {
-        const data = {
-          lat: position.latitude,
-          lng: position.longitude, 
-          speed: navigator.connection.downlink
-        };
-
-        doAjaxPost('/add', data, (response) => {
-          giveConfirmation(response.success, data.speed, '');
-        });
-      },
-      // call this function if failed to get location
-      () => {
-        giveConfirmation(false, 0, 'Unable to retrieve your location.')
-      });
-  } else {
-    // call this function if we can't get internet speed from browser
-    giveConfirmation(false, 0, 'This is not supported by your browser, please use Chrome.');
-  }
-    
-}
-
-
-// closes the add speed popup, opens the confirmation popup and populates text accordingly
-function giveConfirmation(successful, speed, errorMessage) {
-  $('#add-speed-container').hide();
-  $('#confirmation-container').show();
-  if (successful) {
-    $('#confirmation-title').text('Success!');
-    $('#confirmation-text').text('You data has been added to our map. ' +
-      'Your internet speed is currently ' + speed + ' Mbps. Thank you for your help!');
-  } else {
-    $('#confirmation-title').text('Failed to add your internet speed');
-    $('#confirmation-text').text('Error: ' + errorMessage);
+function createListFromObjs() {
+  const listContainer = $('#list-results');
+  for (let i = 0; i < listItems.length; i++) {
+    htmlString = '<div class="list-result">' +
+                    '<div class="list-pic">' +
+                      '<img src="' + listItems[i].image + '" alt="' + listItems[i].name + '"/>' +
+                    '</div>' +
+                    '<div class="list-result-info">' +
+                      '<h2 class="list-result-title">' + (i+1) + '. ' + listItems[i].name + '</h2>' +
+                      '<p> average speed: <span class="' + listItems[i].color + '-speed">' + listItems[i].speed + '</span> Mbps</p>' +
+                    '</div>' +
+                    '<br style="clear: both;" />' +
+                  '</div>' +
+                  '<br style="clear: left;" />';
+    var div = document.createElement('div');
+    div.innerHTML = htmlString.trim();
+    listContainer.append(div.firstChild);
   }
 }
 
 
-function createListElementFromObj(listObj) {
-  htmlString = '<div class="list-result">' +
-                  '<div class="list-pic">' +
-                    '<img src="' + listObj.img + '" alt="' + listObj.title + '"/>' +
-                  '</div>' +
-                  '<div class="list-result-info">' +
-                    '<h2 class="list-result-title">' + listObj.title + '</h2>' +
-                    '<p> average speed: <span class="' + listObj.color + '-speed">' + listObj.speed + '</span> Mbps</p>' +
-                  '</div>' +
-                  '<br style="clear: both;" />' +
-                '</div>' +
-                '<br style="clear: left;" />';
-  var div = document.createElement('div');
-  div.innerHTML = htmlString.trim();
-  return div.firstChild;
+function createMarkersFromObjs() {
+  // for now only add first 4 list items as markers
+  for (let i = 0; i < 4; i++) {
+    const marker = new google.maps.Marker({
+      position: {lat: listItems[i].lat, lng: listItems[i].lng},
+      icon: ('images/marker-' + (i+1) + '.png'),
+      map: map
+    });
+    markers.push(marker);
+  }
 }
 
 
